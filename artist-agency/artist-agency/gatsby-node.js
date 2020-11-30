77a63724e7cc5502ff193f44652c99264e9ee9ed
Robin <lordminboe@gmail.com>
2020-11-30 12:34:44 +0100

    artist template toegevoegd

--- artist-agency/artist-agency/gatsby-node.js
+++ artist-agency/artist-agency/gatsby-node.js
@@ -1,4 +1,39 @@
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
+const path = require("path");
+const  {graphql} = require("gatsby");
+
+exports.createPages = ({graphql, actions}) => {
+const {createPage} = actions
+const query = graphql(`
+{
+wpcontent{
+  artists {
+    edges  {
+      node  {
+        slug
+        id
+      }
+    }
+  }
+}
+}
+`).then(result => {
+  const artists = result.data.wpcontent.artists.edges;
+  artists.forEach(artists => {
+    const {id, slug} = artists.node;
+    createPage({
+
+      path: slug,
+      component:path.resolve('src/templates/artist.js'),
+      context: {
+        id,
+        slug
+      }
+    })
+
+  });
+})
+}
 
 /* Aan de hand van dit stukje code worden de images vanuit WPgraphql omgezet tot images waarop Gatsby image optimization kan toepassen */
 exports.createResolvers = async ({
